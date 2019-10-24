45c544057289aa55f2a177f305e69734edcbd03e
jpvandy <jpvandy@sandia.gov>
2019-10-24 17:52:52 -0600

    include qos Suite

--- buildsys/bamboo.sh
+++ buildsys/bamboo.sh
@@ -424,6 +424,7 @@ echo " #####################################################"
     # Initialize directory to hold temporary test input files
     rm -Rf ${SST_TEST_INPUTS_TEMP}
     mkdir -p ${SST_TEST_INPUTS_TEMP}
+        ${SST_TEST_SUITES}/testSuite_qos.sh
 
     # Do we run the Macro Tests
     if [ $1 == "sst-macro_withsstcore_mac" ]   || [ $1 == "sst-macro_nosstcore_mac" ] ||
